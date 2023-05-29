d6d5792c81bd146becdfcb4ada5aec27e7ca4963
Tri <ductri.vu@gmail.com>
2023-05-29 11:46:38 +0200

    classes, initializers

---
+++ 3 Objects…get oriented Making code make sense/PickACardUI/CardPicker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACardUI
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+
+        /// <summary>
+        /// PIcks a number of CARDS & returns THEM 
+        /// </summary>
+        /// <param name="numberOfCards">number of cards to pick (surprised ?)</param>
+        /// <returns>An array of strings that contain the card names</returns>
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = numberOfCards - 1; i >= 0; i--)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            // get a random number from 1 to 4 !
+            int value = random.Next(1, 5);
+
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            // get a random number from 1 to 13
+            int value = random.Next(1, 14);
+
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+
+            return value.ToString();
+        }
+
+    }
+
+}
---
+++ 3 Objects…get oriented Making code make sense/PickRAndomCards/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PickRAndomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        START:
+
+            Console.WriteLine("Number of cards to pick ?");
+            string answer = Console.ReadLine();
+
+            if (int.TryParse(answer, out int numberOfCards))
+            {
+                // this block is executed if line COULD be converted to an int
+                // value that's stored in a new variable called numberOfCards
+                string[] cards = CardPicker.PickSomeCards(numberOfCards);
+                foreach (string card in cards)
+                {
+                    Console.WriteLine($"{card}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("INVALID INPUT, YOU MUST ENTER A NUMBER !");
+                goto START;
+            }
+
+
+        }
+    }
+}
