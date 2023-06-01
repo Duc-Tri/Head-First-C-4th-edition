7da88c10e3179435079ebf14535d8f9dc5b30dc2
Tri <ductri.vu@gmail.com>
2023-06-02 01:10:06 +0200

    auto-properties

---
+++ 5 Encapsulation Keep your privates private/SwordDamage/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SwordDamage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SwordDamageCalculator swordDamageCalculator = new SwordDamageCalculator();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamageCalculator.Magic = cbMagic.IsChecked.Value;
+            swordDamageCalculator.Flaming = cbMagic.IsChecked.Value;
+            RollDice();
+
+            FieldInfo[] fields = swordDamageCalculator.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (FieldInfo field in fields)
+            {
+                Console.WriteLine(field.Name + " => " + field.GetValue(swordDamageCalculator));
+            }
+        }
+
+        private void RollDice()
+        {
+            swordDamageCalculator.RollDice();
+            DisplayDamage();
+        }
+
+        private void DisplayDamage()
+        {
+            Damage_Text.Text = "Rolled " + swordDamageCalculator.Roll + " for " + swordDamageCalculator.Damage + " HP\n" + swordDamageCalculator.ToString();
+        }
+
+        private void Magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamageCalculator.Magic = true;
+            DisplayDamage();
+        }
+
+        private void Magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamageCalculator.Magic = false;
+            DisplayDamage();
+        }
+
+        private void Flaming_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamageCalculator.Flaming = true;
+            DisplayDamage();
+        }
+
+        private void Flaming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamageCalculator.Flaming = false;
+            DisplayDamage();
+        }
+
+        private void Roll_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+
+    }
+}
---
+++ 5 Encapsulation Keep your privates private/SwordDamage/SwordDamageCalculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SwordDamage
+{
+    public class SwordDamageCalculator
+    {
+        Random random = new Random();
+
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll { get; private set; }
+
+        private decimal MagicMultiplier = 1M;
+        private int FlamingDamage = 0;
+
+        public int Damage
+        {
+            get
+            {
+                return (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            }
+        }
+
+        public bool Magic
+        {
+            set
+            {
+                MagicMultiplier = (value ? 1.75M : 1M);
+
+                Debug.WriteLine($"Magic finished: {Damage} (roll: {Roll})");
+            }
+        }
+
+        public bool Flaming
+        {
+            set
+            {
+                FlamingDamage = (value ? FLAME_DAMAGE : 0);
+
+                Debug.WriteLine($"Flaming finished: {Damage} (roll: {Roll})");
+            }
+        }
+
+        internal void RollDice()
+        {
+            Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+
+            Debug.WriteLine($"RollDice finished: {Damage} (roll: {Roll}");
+        }
+
+        public string ToString()
+        {
+            return $" ( {Roll} * {MagicMultiplier} = {(int)(Roll * MagicMultiplier)}) + {BASE_DAMAGE} + {FlamingDamage}";
+        }
+
+    }
+
+}
