99fa1d1bebcae4cafaa9f0098ea51e284b208dd4
Tri <ductri.vu@gmail.com>
2023-07-04 01:07:47 +0200

    interface

---
+++ 7 interfaces, casting, and “is”/TallGuy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallGuy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy() { Height = 76, Name = "Jimmy" };
+            tallGuy.TalkAboutYourself();
+            Console.WriteLine($"The tall guy has{tallGuy.FunnyThingIHave}");
+            tallGuy.Honk();
+        }
+    }
+}
---
+++ 7 interfaces, casting, and “is”/TallGuy/TallGuy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallGuy
+{
+    internal class TallGuy : IClown
+    {
+        public String Name;
+        public int Height;
+
+        public string FunnyThingIHave => "big shoes";
+
+        public void Honk()
+        {
+            Console.WriteLine("Honk honk !");
+        }
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
+        }
+    }
+}
