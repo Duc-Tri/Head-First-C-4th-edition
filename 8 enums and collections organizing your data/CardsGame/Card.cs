79968deea909daa360a0c0e6bafde33d9881eeb4
Tri <ductri.vu@gmail.com>
2023-07-08 00:48:36 +0200

    rearrange folders structure (for futur exercices, including those found on Github)

---
+++ 8 enums and collections organizing your data/CardsGame/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsGame
+{
+
+    internal class Card
+    {
+        readonly Suits Suit;
+        readonly Values Value;
+        public string Name
+        {
+            get
+            {
+                //return Value + " of " + Suit;
+                return $"{Value} of {Suit}";
+            }
+        }
+
+        public Card()
+        { }
+
+        public Card(Suits suit, Values value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+    }
+}
---
+++ 8 enums and collections organizing your data/CardsGame/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CardsGame;
+
+Console.WriteLine("==================== HOUSE OF CARDS");
+
+Console.WriteLine((new Card(Suits.Clubs, Values.Ace)).Name);
+
+Random random = new Random();
+
+for (int i = 0; i < 20; i++)
+{
+    Console.WriteLine(i + "] " + (new Card((Suits)random.Next(4), (Values)random.Next(1, 14))).Name);
+}
