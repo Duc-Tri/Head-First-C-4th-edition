e5267aa4d1038ba034c67da063259d5e009e0db2
Tri <ductri.vu@gmail.com>
2023-07-11 14:38:08 +0200

    Two Decks WPF (data binding, ObservanleCollection)

---
+++ 8 enums and collections organizing your data/Two Decks/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Decks
+{
+    internal class Card
+    {
+        public readonly Suits Suit;
+        public readonly Values Value;
+
+        public string Name
+        {
+            get
+            {
+                return $"{Value} of {Suit}";
+            }
+        }
+
+        public Card()
+        {
+        }
+
+        public Card(Suits suit, Values value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ 8 enums and collections organizing your data/Two Decks/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Two_Decks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            (Resources["rightDeck"] as Deck)?.Clear();
+        }
+
+        private void MoveCard(bool leftToRight)
+        {
+            if ((Resources["rightDeck"] is Deck rightDeck) && (Resources["leftDeck"] is Deck leftDeck))
+            {
+                if (leftToRight)
+                {
+                    if (leftDeckListBox.SelectedItem is Card card)
+                    {
+                        leftDeck.Remove(card);
+                        rightDeck.Add(card);
+                    }
+                }
+                else
+                {
+                    if (rightDeckListBox.SelectedItem is Card card)
+                    {
+                        rightDeck.Remove(card);
+                        leftDeck.Add(card);
+                    }
+                }
+            }
+        }
+
+        private void resetLeftDeck_Click(object sender, RoutedEventArgs e)
+        {
+            (Resources["leftDeck"] as Deck)?.Reset();
+        }
+
+        private void shuffleLeftDeck_Click(object sender, RoutedEventArgs e)
+        {
+            (Resources["leftDeck"] as Deck)?.Shuffle();
+        }
+
+        private void clearRightDeck_Click(object sender, RoutedEventArgs e)
+        {
+            (Resources["rightDeck"] as Deck)?.Clear();
+        }
+
+        private void sortRightDeck_Click(object sender, RoutedEventArgs e)
+        {
+            (Resources["rightDeck"] as Deck)?.Sort();
+        }
+
+        private void leftDeckListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                MoveCard(true);
+            }
+        }
+
+        private void leftDeckListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MoveCard(true);
+        }
+
+        private void rightDeckListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                MoveCard(false);
+            }
+
+        }
+
+        private void rightDeckListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MoveCard(false);
+        }
+
+    }
+}
