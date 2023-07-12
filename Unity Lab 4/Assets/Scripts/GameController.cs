23713059e142f8f1a517b5829ef1c01b4bcc12e4
Tri <ductri.vu@gmail.com>
2023-07-12 14:28:08 +0200

    unity UI interface

--- Unity Lab 4/Assets/Scripts/GameController.cs
+++ Unity Lab 4/Assets/Scripts/GameController.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject OneBallPrefab;
+    public TextMeshProUGUI ScoreText;
+    public Button PlayAgainButton;
+    //-------------------------------------------
+    public int Score = 0;
+    public bool GameOver = true;
+    public int NumberOfBalls = 0;
+    public int MaximumBalls = 10;
+
 
     void Start()
     {
@@ -13,11 +23,38 @@ public class GameController : MonoBehaviour
 
     void AddABall()
     {
-        Instantiate(OneBallPrefab);
+        if (!GameOver)
+        {
+            Instantiate(OneBallPrefab);
+            NumberOfBalls++;
+            if (NumberOfBalls >= MaximumBalls)
+            {
+                GameOver = true;
+                PlayAgainButton.gameObject.SetActive(true);
+            }
+        }
     }
 
-    void Update()
+    public void ClickOnBall()
     {
-
+        Score++;
+        NumberOfBalls--;
     }
+
+    public void StartGame()
+    {
+        foreach (GameObject ball in GameObject.FindGameObjectsWithTag("Ball"))
+            Destroy(ball);
+
+        PlayAgainButton.gameObject.SetActive(false);
+        Score = 0;
+        NumberOfBalls = 0;
+        GameOver = false;
+    }
+
+    private void Update()
+    {
+        ScoreText.text = Score.ToString();
+    }
+
 }
--- Unity Lab 4/Assets/Scripts/OneBallBehaviour.cs
+++ Unity Lab 4/Assets/Scripts/OneBallBehaviour.cs
@@ -1,34 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class OneBallBehaviour : MonoBehaviour
 {
+    private bool RotateFunction = true;
+    //-----------------------------------------
     public float XRotation = 0;
     public float YRotation = 1;
     public float ZRotation = 0;
     public float DegreesPerSecond = 180;
 
+    //-----------------------------------------
+    public float XSpeed;
+    public float YSpeed;
+    public float ZSpeed;
+    public float Multiplier = 0.75F;
+    public int TooFar = 5;
+
+    //-----------------------------------------
     [SerializeField]
     private int BallNumber;
 
-    private static int staticNumber = 0;
+    private static int BallCount = 0;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        BallNumber = staticNumber++;
+        BallNumber = BallCount++;
+        RotateFunction = (Random.value > 0.5f);
 
-        transform.position = new Vector3(3 - Random.value * 6,
-            3 - Random.value * 6,
-            3 - Random.value * 6);
+        ResetBall();
+    }
+
+    void ResetBall()
+    {
+        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
+
+        XSpeed = Multiplier - Random.value * Multiplier * 2;
+        YSpeed = Multiplier - Random.value * Multiplier * 2;
+        ZSpeed = Multiplier - Random.value * Multiplier * 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (RotateFunction)
+            RotateAround();
+        else
+            Translate();
+    }
+
+    private void Translate()
+    {
+        transform.Translate(Time.deltaTime * XSpeed, Time.deltaTime * YSpeed, Time.deltaTime * ZSpeed);
+
+        XSpeed += Multiplier - Random.value * Multiplier * 2;
+        YSpeed += Multiplier - Random.value * Multiplier * 2;
+        ZSpeed += Multiplier - Random.value * Multiplier * 2;
+
+        if ((Mathf.Abs(transform.position.x) > TooFar) ||
+            (Mathf.Abs(transform.position.y) > TooFar) ||
+            (Mathf.Abs(transform.position.z) > TooFar))
+        {
+            ResetBall();
+        }
+    }
+
+    private void RotateAround()
+    {
         Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
         transform.RotateAround(Vector3.zero, axis, DegreesPerSecond * Time.deltaTime);
+    }
 
+    private void OnMouseDown()
+    {
+        GameController controller = Camera.main.gameObject.GetComponent<GameController>();
+        if (!controller.GameOver)
+        {
+            controller.ClickOnBall();
+            Destroy(gameObject);
+        }
     }
 }
