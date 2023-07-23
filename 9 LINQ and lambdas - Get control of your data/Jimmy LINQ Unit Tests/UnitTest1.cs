318b9aa23679f5a3dcc34b4753861d1b6bda61d3
Tri <ductri.vu@gmail.com>
2023-07-23 02:23:08 +0200

    start Unit tests + update Visual Studio

---
+++ 9 LINQ and lambdas - Get control of your data/Jimmy LINQ Unit Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jimmy_LINQ_Unit_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
