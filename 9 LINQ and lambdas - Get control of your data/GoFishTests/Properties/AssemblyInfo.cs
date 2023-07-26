c779f813aa0311f00cf3e10b4a7915d5d84c1f86
Tri <ductri.vu@gmail.com>
2023-07-26 03:40:43 +0200

    resolving AT LAST this f* "restore Nuget packages" and "loading Intellisense" stalling when updating MSTest to .NET6.0

---
+++ 9 LINQ and lambdas - Get control of your data/GoFishTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoFishTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoFishTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b927a9b6-82e6-416f-9a73-4d3046118174")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 9 LINQ and lambdas - Get control of your data/TESTS/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3df87da-ad8a-4e09-8939-2f58a309cdd0")]
