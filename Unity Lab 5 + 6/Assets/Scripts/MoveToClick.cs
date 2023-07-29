11dd1d40df790323c4da5bf32a6fc4eba8dd761f
Tri <ductri.vu@gmail.com>
2023-07-29 19:43:30 +0200

    Unity Lab 6 finished (NavMeshObstacle carving, NavMeshAgent height, ManMeshSurface bake)

--- Unity Lab 5 + 6/Assets/Scripts/MoveToClick.cs
+++ Unity Lab 5 + 6/Assets/Scripts/MoveToClick.cs
@@ -7,23 +7,27 @@ using UnityEngine.XR;
 public class MoveToClick : MonoBehaviour
 {
     private NavMeshAgent agent;
-    Camera mainCamera;
+    public Camera mainCamera;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-        mainCamera = Camera.main;
+        if (mainCamera == null)
+            mainCamera = Camera.main;
     }
 
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            Ray ray=mainCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit; 
-            if(Physics.Raycast(ray, out hit,100)) {
-                agent.SetDestination(hit.point);
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (!hit.collider.CompareTag("Obstacle"))
+                    agent.SetDestination(hit.point);
             }
         }
     }
+
 }
