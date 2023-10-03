8fb5b79eb51c76d7071aef1159244385571ad9de
Tri <ductri.vu@gmail.com>
2023-10-04 00:56:51 +0200

    Binary stream test

---
+++ 10 reading and writing files/Binary Stream/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Stream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=====BinaryWriterTest");
+            BinaryWriterTest();
+            Console.WriteLine("=====BinaryReaderTest");
+            BinaryReaderTest();
+            Console.WriteLine("=====BinaryReaderTest2");
+            BinaryReaderTest2();
+
+        }
+
+
+
+        static void BinaryWriterTest()
+        {
+            int intValue = 48769414;
+            string stringValue = "Hello!";
+            byte[] byteArray = { 47, 129, 0, 116 };
+            float floatValue = 491.695F;
+            char charValue = 'E';
+
+            // If you use File.Create, it’ll start a new file—if there’s one there already, it’ll blow it away and start a brand-new one.The File.OpenWrite method opens the existing one and starts overwriting it from the beginning instead.
+            using (var output = File.Create("binarydata.dat"))
+            using (var writer = new BinaryWriter(output))
+            {
+                writer.Write(intValue);
+                writer.Write(stringValue);
+                writer.Write(byteArray);
+                writer.Write(floatValue);
+                writer.Write(charValue);
+
+                // Each Write statement encodes one value into bytes, and then sends those bytes to the FileStream object.You can pass it any value type, and it’ll encode it automatically.
+            }
+
+        }
+
+        static void BinaryReaderTest()
+        {
+            byte[] dataWritten = File.ReadAllBytes("binarydata.dat");
+            foreach (byte b in dataWritten) Console.Write("{0:x2} ", b);
+            Console.WriteLine(" - {0} bytes", dataWritten.Length);
+
+            // 86 29 e8 02 06 48 65 6c 6c 6f 21 2f 81 00 74 f6 d8 f5 43 45  - 20 bytes
+            // float and int values take up four bytes when you write them to a file. If you’d used long or double, then they’d take up eight bytes each.
+
+        }
+
+        static void BinaryReaderTest2()
+        {
+            using (var input = File.OpenRead("binarydata.dat"))
+            using (var reader = new BinaryReader(input))
+            {
+                // BinaryReader that returns the data in the correct type.Most don’t need any parameters, but ReadBytes takes one parameter that tells BinaryReader how many bytes to read.
+
+                int intRead = reader.ReadInt32();
+                string stringRead = reader.ReadString();
+                byte[] byteArrayRead = reader.ReadBytes(4);
+                float floatRead = reader.ReadSingle();
+                char charRead = reader.ReadChar();
+
+                Console.Write("int: {0} string: {1} bytes: ", intRead, stringRead);
+                foreach (byte b in byteArrayRead) Console.Write("{0} ", b);
+
+                Console.Write(" float: {0} char: {1} ", floatRead, charRead);
+            }
+
+        }
+    }
+}
+
---
+++ 10 reading and writing files/HexDump/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexDump
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var position = 0;
+            // A StreamReader’s EndOfStream property returns false if there are characters still left to read in the file.
+
+            //using (var reader = new StreamReader("textdata.txt")) // "binarydata.dat"
+            using (Stream input = File.OpenRead(args[0]))
+            {
+                var buffer = new byte[16];
+                int bytesRead;
+                while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    // Read up to the next 16 bytes from the file into a byte array
+
+                    // The {0:x4} formatter converts a numeric value to a four-digit hex number, so 1984 is converted to the string “07c0”.
+                    // Write the position (or offset) in hex, followed by a colon and space
+                    Console.Write("{0:x4}: ", position);
+                    position += bytesRead;
+
+                    // Write the hex value of each character in the byte array
+                    // This loop goes through the characters and prints each of them to a line in the output.
+                    for (var i = 0; i < 16; i++)
+                    {
+                        if (i < bytesRead)
+                            Console.Write("{0:x2} ", (byte)buffer[i]);
+                        else
+                            Console.Write(" ");
+
+                        if (buffer[i] < 0x20 || buffer[i] > 0x7F) buffer[i] = (byte)'.';
+
+                        if (i == 7) Console.Write("-- ");
+                    }
+
+                    // Write the actual characters in the byte array
+                    var bufferContents = Encoding.UTF8.GetString(buffer);
+
+                    // The String.Substring method returns a part of a string.The first parameter is the starting position(in this case, the beginning of the string), and the second is the number of characters to include in the substring. The String class has an overloaded constructor that takes a char array as a parameter and converts it to a string.
+                    Console.WriteLine(" {0}", bufferContents.Substring(0, bytesRead));
+                }
+            }
+        }
+    }
+
+}
