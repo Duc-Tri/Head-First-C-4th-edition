23b22cc05eb8eaab36d4802d97d9833808e23a48
Tri <ductri.vu@gmail.com>
2023-11-03 00:09:27 +0100

    CH11 Garbage Collector / finalizer

---
+++ 11 the death of an object/VariousTests/GCTests/EvilClone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GCTests
+{
+    class EvilClone
+    {
+        public static int CloneCount = 0;
+        public int CloneID { get; } = ++CloneCount;
+        public EvilClone() => Console.WriteLine("Clone #{0} is wreaking havoc", CloneID);
+
+// In general, you’ll never write a finalizer for an object that only owns managed resources. Everything you’ve encountered so far in this book has been managed by the CLR. But occasionally programmers need to access a Windows resource that isn’t in a .NET namespace. For example, if you find code on the internet that has[DllImport] above a declaration, you might be using an unmanaged resource.And some of those non-.NET resources might leave your system unstable if they’re not “cleaned up.” That’s what finalizers are for. This is the finalizer (sometimes called a “destructor”). It’s declared as a method that starts with a tilde ~and has no return value or parameters. An object’s  finalizer is run just before the object is garbage-collected. 
+        ~EvilClone()
+        {
+            Console.WriteLine("Clone #{0} destroyed", CloneID);
+        }
+    }
+}
+
+
---
+++ 11 the death of an object/VariousTests/GCTests/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GCTests
+{
+    // The Main method instantiates EvilClone objects, dereferences (or removes references to) them, and collects them:
+    class Program
+    {
+        // Run your app and press a a few times to create some EvilClone objects and add them to the List. Then press c to clear the List and remove all references to those EvilClone objects. Press c a few times—there’s a small chance the CLR will collect some of the objects that were dereferenced, but you probably won’t see them collected until you press g to call GC.Collect.
+
+        static void Main(string[] args)
+        {
+            // We’ll use Stopwatch to get an idea of how fast garbage collection runs.The Stopwatch class lets you accurately measure elapsed time by starting a new stopwatch and getting the number of milliseconds elapsed since you started it.
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+            var clones = new List<EvilClone>();
+            while (true)
+            {
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    // When you press the ‘a’ key your app creates a new instance of EvilClone and adds it to the clones List.
+                    case 'a':
+                        clones.Add(new EvilClone());
+                        break;
+                    // Pressing ‘c’ tells the app to clear the List, removing all references to—or dereferencing—all of the clones that you instantiated and added.
+                    case 'c':
+                        Console.WriteLine("Clearing list at time {0}", stopwatch.ElapsedMilliseconds);
+                        clones.Clear();
+                        break;
+                    // Pressing ‘g’ tells the CLR to collect all objects that have been marked for garbage collection.
+                    case 'g':
+                        Console.WriteLine("Collecting at time {0}", stopwatch.ElapsedMilliseconds);
+                        GC.Collect();
+                        break;
+                };
+            }
+        }
+
+
+    }
+}
