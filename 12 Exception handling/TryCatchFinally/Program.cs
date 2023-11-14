5f88ac6743e1dad9be13fa1c1a270486c91c8384
Tri <ductri.vu@gmail.com>
2023-11-15 00:12:44 +0100

    continuing

--- 12 Exception handling/TryCatchFinally/Program.cs
+++ 12 Exception handling/TryCatchFinally/Program.cs
@@ -14,20 +14,33 @@ namespace TryCatchFinally
             {
                 var lines = File.ReadAllLines(args[0]);
                 firstLine = (lines.Length > 0) ? lines[0] : "The file was empty";
-                int j=0;
+                int j = 0;
                 int i = 3 / j;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.Error.WriteLine(" ! Please specify a filename.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.Error.WriteLine(" ! Unable to find file: {0}", args[0]);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.Error.WriteLine(" ! File {0} could not be accessed: {1}",
+                args[0], ex.Message);
             }
             catch (System.DivideByZeroException e)
             {
-                Console.Error.WriteLine("DivideByZeroException", e);
+                Console.Error.WriteLine(" ! DivideByZeroException", e);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                Console.Error.WriteLine("Could not read lines from the file: {0}", ex);
+                Console.Error.WriteLine(" ! AVOID CATCH-ALL EXCEPTION !");
             }
             finally
             {
-                Console.WriteLine(firstLine);
+                Console.WriteLine(" ■ FINALLY " + firstLine);
             }
 
             Console.WriteLine(" ■ this line is after the finally{}, happened if no Exception or when a catch{} triggered ! ");
