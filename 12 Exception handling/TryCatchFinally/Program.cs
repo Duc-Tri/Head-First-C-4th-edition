a850902c07c5f6acd5e99681cf826bc11f22b639
Tri <ductri.vu@gmail.com>
2023-11-15 23:57:20 +0100

    add more tests

--- 12 Exception handling/TryCatchFinally/Program.cs
+++ 12 Exception handling/TryCatchFinally/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq.Expressions;
 
@@ -8,11 +9,25 @@ namespace TryCatchFinally
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Factorielle(60));
+            Dummy d = new Dummy();
+            Console.WriteLine(d);
+            Console.WriteLine(d.lints);
+            TryCatchFinallyTest(null);
+        }
+
+        private static ulong Factorielle(ulong v)
+        {
+            return (v > 1) ? (v * Factorielle(v - 1)) : 1;
+        }
+
+        static void TryCatchFinallyTest(string filename)
+        {
             var firstLine = "No first line was read";
 
             try
             {
-                var lines = File.ReadAllLines(args[0]);
+                var lines = File.ReadAllLines(filename); // args[0]
                 firstLine = (lines.Length > 0) ? lines[0] : "The file was empty";
                 int j = 0;
                 int i = 3 / j;
@@ -23,12 +38,12 @@ namespace TryCatchFinally
             }
             catch (FileNotFoundException)
             {
-                Console.Error.WriteLine(" ! Unable to find file: {0}", args[0]);
+                Console.Error.WriteLine(" ! Unable to find file: {0}", filename); // args[0]
             }
             catch (UnauthorizedAccessException ex)
             {
                 Console.Error.WriteLine(" ! File {0} could not be accessed: {1}",
-                args[0], ex.Message);
+                filename, ex.Message); // args[0]
             }
             catch (System.DivideByZeroException e)
             {
@@ -40,10 +55,32 @@ namespace TryCatchFinally
             }
             finally
             {
-                Console.WriteLine(" ■ FINALLY " + firstLine);
+                Console.WriteLine(" ■ FINALLY : " + firstLine);
             }
 
-            Console.WriteLine(" ■ this line is after the finally{}, happened if no Exception or when a catch{} triggered ! ");
+            Console.WriteLine(" ■ this line is after the finally{}, happened if no Exception or when a catch{} triggered !");
+        }
+
+    }
+
+    public class Dummy
+    {
+        public List<int> lints;
+
+        public Dummy()
+        {
+            try
+            {
+                int i = 0;
+                i = 1 / i;
+            }
+            catch // catch-all
+            {
+            }
+            finally
+            {
+                lints = new List<int>();
+            }
         }
     }
 }
