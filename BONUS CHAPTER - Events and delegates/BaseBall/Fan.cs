996a54b39da95f4f7960269d3f64b0332ca90efa
Tri <ductri.vu@gmail.com>
2024-03-07 19:55:38 +0100

    BONUS CHAPTER - Events and delegates - Ball, BallEventArgs, Pitcher, Fan

---
+++ BONUS CHAPTER - Events and delegates/BaseBall/Fan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BaseBall
+{
+    internal class Fan
+    {
+        private int pitchNumber = 0;
+        public Fan(Ball ball) => ball.BallInPlay += BallInPlayEventHandler;
+
+        private void BallInPlayEventHandler(object sender, System.EventArgs e)
+        {
+            pitchNumber++;
+            if (e is BallEventArgs ballEventArgs)
+            {
+                // The fan’s BallInPlay event handler looks for any ball that’s high and long.
+                if (ballEventArgs.Distance > 400 && ballEventArgs.Angle > 30)
+                    Console.WriteLine($"Fan : #{pitchNumber} Homerun ! I'm going for the ball !");
+                else
+                    Console.WriteLine($"Fan : #{pitchNumber} Not A Homerun !");
+            }
+        }
+    }
+}
+
