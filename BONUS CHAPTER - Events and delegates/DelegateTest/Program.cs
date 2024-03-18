c0e2316f842d991c15ab6021de8efa66d2bb3658
Tri <ductri.vu@gmail.com>
2024-03-18 23:53:42 +0100

    BONUS CHAPTER - Events and delegates - Tests delegates -> p25

---
+++ BONUS CHAPTER - Events and delegates/DelegateTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DelegateTest
+{
+    internal class Program
+    {
+        delegate string IntToString(int i);
+
+        public static string AddNumberSign(int i) => $"#{i}";
+
+        public static string PlusOne(int i) => $"{i} plus one equals {i + 1}";
+
+        static void Main(string[] args)
+        {
+            // You pointed the methodRef variable to your AddNumberSign method, then you used it to call the method and printed its return value.
+            IntToString methodRef = AddNumberSign;
+            Console.WriteLine(methodRef(12345));
+
+            // You changed methodRef to point to your PlusOne method, so now when you call methodRef(12345) it calls PlusOne instead of AddNumberSign.
+            methodRef = PlusOne;
+            Console.WriteLine(methodRef(12345));
+        }
+
+    }
+
+}
