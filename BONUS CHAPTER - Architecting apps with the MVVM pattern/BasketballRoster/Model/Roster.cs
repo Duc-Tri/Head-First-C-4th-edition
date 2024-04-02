2c22fa78252a4fe4e85ab5f6b091a34956fce3e9
Tri <ductri.vu@gmail.com>
2024-04-02 23:51:33 +0200

    BONUS CHAPTER - WPF / MVVM pattern - BasketballRoster (page 9)

---
+++ BONUS CHAPTER - Architecting apps with the MVVM pattern/BasketballRoster/Model/Roster.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BasketballRoster.Model
+{
+    class Roster
+    {
+        public string TeamName { get; private set; }
+
+        private readonly List<Player> _players = new List<Player>();
+        public IEnumerable<Player> Players
+        {
+            get => new List<Player>(_players);
+        }
+
+        public Roster(string teamName, IEnumerable<Player> players)
+        {
+            TeamName = teamName;
+            _players.AddRange(players);
+        }
+    }
+}
