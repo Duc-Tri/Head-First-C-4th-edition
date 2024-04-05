b83aa7703706758ed5967d064f3cd40acd2f9eaa
Tri <ductri.vu@gmail.com>
2024-04-05 23:52:52 +0200

    BONUS CHAPTER - WPF / MVVM pattern - add an user control (RosterControl)

---
+++ BONUS CHAPTER - Architecting apps with the MVVM pattern/BasketballRoster/View/RosterControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace BasketballRoster.View
+{
+    /// <summary>
+    /// Interaction logic for RosterControl.xaml
+    /// </summary>
+    public partial class RosterControl : UserControl
+    {
+        public RosterControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
